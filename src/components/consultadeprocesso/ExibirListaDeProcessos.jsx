c7f906a52b62029f42e3a44b405f705d6d1bdd4b
Brayan Moncks <brayanbmn@hotmail.com>
2021-02-07 20:42:10 -0300

    Alinhamento da lista de processos

--- src/components/consultadeprocesso/ExibirListaDeProcessos.jsx
+++ src/components/consultadeprocesso/ExibirListaDeProcessos.jsx
@@ -3,13 +3,28 @@ import './ExibirListaDeProcessos.css';
 import Titulo from '../home/Titulo';
 import Busca from '../home/Busca';
 import BotaoNovo from './BotaoNovo';
-import { withRouter } from 'react-router-dom';
+import Cards from '../layout/Card';
+import icon from '../img/iconfinder_outlined_placeholder_4280497.png';
 
 export default class ExibirListaDeProcessos extends Component {
   state = {
     processo: this.props.location.exibirProps.processo,
+    processos: [],
+  };
+  getProcessos = () => {
+    var searchUrl = `http://localhost:3002/processo?q=${this.state.processo}`;
+    fetch(searchUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonData) => {
+        this.setState({ processos: jsonData });
+      });
   };
 
+  componentDidMount() {
+    this.getProcessos();
+  }
   render() {
     return (
       <>
@@ -43,7 +58,38 @@ export default class ExibirListaDeProcessos extends Component {
             Novo
           </BotaoNovo>
         </div>
-        <div id="containerProcesso"></div>
+
+        <div>
+          <ul className="lista">
+            {this.state.processos.map((processo, index) => (
+              <Cards style="maxWidth: 600px">
+                <div class="lista-processos" key={index}>
+                  <li id="single-processo">
+                    <div id="img">
+                      <img src={icon} alt="empty icon" style={{ width: 100 }} />
+                    </div>
+                    <div class="item-processo">
+                      <label id="li-label">Número</label>
+                      <p id="paragrafo">{processo.numero}</p>
+                    </div>
+                    <div class="item-processo">
+                      <label id="li-label">Assunto</label>
+                      <p id="paragrafo">{processo.assunto}</p>
+                    </div>
+                    <div class="item-processo">
+                      <label id="li-label">Interessado</label>
+                      <p id="paragrafo">{processo.interessados}</p>
+                    </div>
+                    <div class="item-processo">
+                      <label id="li-label">Descrição</label>
+                      <p id="paragrafo">{processo.descricao}</p>
+                    </div>
+                  </li>
+                </div>
+              </Cards>
+            ))}
+          </ul>
+        </div>
       </>
     );
   }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -61,7 +61,7 @@ export default class Home extends Component {
                 },
               }}
             >
-              Sign up
+              Buscar
             </Link>
           </Botao>
         </div>
